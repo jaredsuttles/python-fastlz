f69114f3bc148cd4a0d70eea9c173e4f093caa78
F M <>
2023-07-20 22:11:08 +0200

    fix for python 3.10
    Addressing this annoying issue:
    - SystemError: PY_SSIZE_T_CLEAN macro must be defined for '#' formats
    - A crash due to Py_ssize_t having different size than int

--- fastlz.c
+++ fastlz.c
@@ -1,3 +1,4 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include "fastlz/fastlz.h"
 
@@ -16,7 +17,7 @@ compress(PyObject *self, PyObject *args, PyObject *kwargs)
     PyObject *result;
     const char *input;
     char *output;
-    int level = 0, input_len, output_len;
+    Py_ssize_t level = 0, input_len, output_len;
 
     static char *arglist[] = {"string", "level", NULL};
     if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s#|i", arglist, &input,
@@ -62,9 +63,9 @@ decompress(PyObject *self, PyObject *args)
 {
     PyObject *result;
     const char *input;
-    int input_len;
+    Py_ssize_t input_len;
     char *output;
-    uint32_t output_len, decompressed_len;
+    Py_ssize_t output_len, decompressed_len;
 
     if (!PyArg_ParseTuple(args, "s#", &input, &input_len))
         return NULL;
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup, Extension
 
 setup(
     name='fastlz',
+    python_requires='>=3.10.0',
     version='0.0.2',
     description='Python wrapper for FastLZ, a lightning-fast lossless '
                 'compression library.',
